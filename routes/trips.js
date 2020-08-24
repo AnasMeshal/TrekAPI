280d55b9c386fb029a2eb692a5e9469b5fd7155d
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-24 22:08:06 +0300

    Linked trip routes with controllers and exported

---
+++ routes/trips.js
@@ -0,0 +1,38 @@
+const express = require("express");
+//Controllers
+const {
+  tripList,
+  tripCreate,
+  tripUpdate,
+  tripDelete,
+  fetchTrip,
+} = require("../controllers/tripController");
+
+const router = express.Router();
+
+//Param
+router.param("tripId", async (req, res, next, tripId) => {
+  const trip = await fetchTrip(tripId, next);
+  if (trip) {
+    req.trip = trip;
+    next();
+  } else {
+    const err = new Error("Trip Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+//List
+router.get("/", tripList);
+
+//Create
+router.post("/", tripCreate);
+
+//Update
+router.put("/:tripId", tripUpdate);
+
+//Delete
+router.delete("/:tripId", tripDelete);
+
+module.exports = router;
