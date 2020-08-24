5005afdf8a4d0ac98808a24dbbd58537ac1a1932
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-08-24 21:58:15 +0300

    set up db, importedand used cors/bodyParser, fixed db import to index.js within db folder imported and used trip routes changed db.authenticate to db.sync

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const db = require("./db/db");
+//Database
+const db = require("./db");
+
+//Routes
+const tripRoutes = require("./routes/trips");
 
 const app = express();
 
+app.use(cors());
+app.use(bodyParser.json());
+
+//Routers
+app.use("/trips", tripRoutes);
+
 const run = async () => {
   try {
-    await db.authenticate();
+    await db.sync();
     console.log("Connection to the database successful!");
   } catch (error) {
     console.error("Error connecting to the database: ", error);
