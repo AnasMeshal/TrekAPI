4109003c545d636b83cf6d2a48d36a3c08b795f9
Sarah <S00038939@auk.edu.kw>
2020-08-25 17:46:33 +0300

    added signin function and payloads

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,18 +1,50 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { JWT_EXPIRATION_MS, JWT_SECRET } = require("./config/keys");
 
 // Database
 const { User } = require("../db/models");
 
 exports.signup = async (req, res, next) => {
-  const { password } = req.body;
-  const saltRounds = 10;
   try {
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
-    // console.log("exports.signup -> hashedPassword", hashedPassword);
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds); // req.body has the password
     req.body.password = hashedPassword;
     const newUser = await User.create(req.body);
-    res.status(201).json({ message: "User created successfully" });
+
+    const payload = {
+      id: newUser.id,
+      email: newUser.email,
+      firstName: newUser.firstName,
+      lastName: newUser.lastName,
+      username: newUser.username,
+
+      // role: newUser.role,
+
+      exp: Date.now() + JWT_EXPIRATION_MS,
+    };
+
+    const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+
+    res.status(201).json({ token });
   } catch (error) {
     next(error);
   }
+};
+
+exports.signin = async (req, res, next) => {
+  const { user } = req;
+  //const shop = await Shop.findOne({ where: { userId: user.id } });
+  const payload = {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    role: user.role,
+    //shopSlug: shop ? shop.slug : null,
+    exp: Date.now() + JWT_EXPIRATION_MS,
+  };
+  const token = jwt.sign(JSON.stringify(payload), JWT_SECRET);
+  res.json({ token });
 };
