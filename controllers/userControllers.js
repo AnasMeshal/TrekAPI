f3480929bbfd1991f72e5a0f3df9433c2d127f84
Sarah <S00038939@auk.edu.kw>
2020-08-25 15:33:43 +0300

    set up signup route

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,8 +1,7 @@
 exports.signup = async (req, res, next) => {
   try {
-    console.log("Hi");
-    // const newUser = await User.create(req.body);
-    //res.json(newUser);
+    const newUser = await User.create(req.body);
+    res.json(newUser);
   } catch (error) {
     next(error);
   }
