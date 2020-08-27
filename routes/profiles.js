a65a50b3ed42acde7a7883b16c1ed1b132aa3a7e
Sarah <S00038939@auk.edu.kw>
2020-08-27 17:19:24 +0300

    moved tripCreate from profile routes to trip routes; also removed router.params for the profile routes

--- routes/profiles.js
+++ routes/profiles.js
@@ -6,31 +6,24 @@ const passport = require("passport");
 const {
   fetchProfile,
   profileUpdate,
-  tripCreate,
 } = require("../controllers/profileController");
 
-router.param("profileId", async (req, res, next, profileId) => {
-  const profile = await fetchProfile(profileId, next);
-  if (profile) {
-    req.profile = profile;
-    next();
-  } else {
-    const error = new Error("Profile Not Found");
-    error.status = 404;
-    next(error);
-  }
-});
+// router.param("profileId", async (req, res, next, profileId) => {
+//   const profile = await fetchProfile(profileId, next);
+//   if (profile) {
+//     req.profile = profile;
+//     next();
+//   } else {
+//     const error = new Error("Profile Not Found");
+//     error.status = 404;
+//     next(error);
+//   }
+// });
 
 router.put(
-  "/:profileId",
+  "/",
   passport.authenticate("jwt", { session: false }),
   profileUpdate
-);
-
-router.post(
-  "/:profileId/trips",
-  passport.authenticate("jwt", { session: false }),
-  tripCreate
 );
 
 module.exports = router;
