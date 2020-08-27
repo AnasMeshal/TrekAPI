cf7b2171a8df3d49ae6c632a8c015d684b4145e2
Sarah <S00038939@auk.edu.kw>
2020-08-27 17:25:26 +0300

    removed router patams from the profiles route

--- routes/profiles.js
+++ routes/profiles.js
@@ -3,22 +3,7 @@ const router = express.Router();
 const passport = require("passport");
 
 //Controller
-const {
-  fetchProfile,
-  profileUpdate,
-} = require("../controllers/profileController");
-
-// router.param("profileId", async (req, res, next, profileId) => {
-//   const profile = await fetchProfile(profileId, next);
-//   if (profile) {
-//     req.profile = profile;
-//     next();
-//   } else {
-//     const error = new Error("Profile Not Found");
-//     error.status = 404;
-//     next(error);
-//   }
-// });
+const { profileUpdate } = require("../controllers/profileController");
 
 router.put(
   "/",
