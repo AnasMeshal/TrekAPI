aa25928901225cc28267e928c21dbdaecd899dee
Yousif Alozairi <Yousif@Ouss-MBP.localdomain>
2020-09-01 18:50:11 +0300

    read and cleared mshary's important valuable amazing comments

--- db/db.js
+++ db/db.js
@@ -2,11 +2,6 @@ const { Sequelize } = require("sequelize");
 
 const db = new Sequelize({
   username: "postgres",
-  // youre using the same PW and DB name, so this works out.
-  // but you should remove PW and DB name before pushing, and
-  // add them when you pull.
-  // leave this for now, but im adding this comment for your info
-  // on real life projects, you shouldn't push your DB PW to GitHub
   password: "Trek",
   database: "TrekDB",
   dialect: "postgres",
