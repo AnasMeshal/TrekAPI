fef9bf60def0a9e85d8ce716d46a58f240ff8814
AnasMeshal <Fire-Ball-KwT@hotmail.com>
2020-09-02 21:39:45 +0300

    created a route for the lists

---
+++ routes/lists.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const passport = require("passport");
+
+// Controllers
+const {
+  fetchList,
+  listList,
+  listCreate,
+  listUpdate,
+  listDelete,
+  AddTripToList,
+  DeleteTripFromList,
+} = require("../controllers/listController");
+
+const router = express.Router();
+
+// Param
+router.param("listId", async (req, res, next, listId) => {
+  const list = await fetchList(listId, next);
+  if (list) {
+    req.list = list;
+    next();
+  } else {
+    const err = new Error("List Not Found");
+    err.status = 404;
+    next(err);
+  }
+});
+
+// List
+router.get("/", listList);
+
+// Create
+router.post("/", passport.authenticate("jwt", { session: false }), listCreate);
+
+// Update
+router.put(
+  "/:listId",
+  passport.authenticate("jwt", { session: false }),
+  listUpdate
+);
+
+// Delete
+router.delete(
+  "/:listId",
+  passport.authenticate("jwt", { session: false }),
+  listDelete
+);
+
+// Add a trip to the list
+router.post(
+  "/:listId/trips",
+  passport.authenticate("jwt", { session: false }),
+  AddTripToList
+);
+
+router.delete(
+  "/:listId/trips",
+  passport.authenticate("jwt", { session: false }),
+  DeleteTripFromList
+);
+
+module.exports = router;
